6c2873c9e5cb5a52d69160e5187f150b4c5a3a05
LetsCodeManh <manh.ng@icloud.com>
2023-01-20 23:51:13 +0100

    Prepare testing for Event and NumberOfEvents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import CitySearch from "./components/CitySearch/CitySearch";
 import EventList from "./components/EventList/EventList";
+import NumberOfEvents from "./components/NumberOfEvents/NumberOfEvents";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CitySearch />
+        <NumberOfEvents />
         <EventList />
       </div>
     );
