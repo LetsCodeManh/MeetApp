e6af2e3652cb2ef3495bb54e0f2576e7af1a143c
LetsCodeManh <manh.ng@icloud.com>
2023-01-22 16:19:32 +0100

    Testing App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React, { Component } from "react";
 import CitySearch from "./components/CitySearch/CitySearch";
 import NumberOfEvents from "./components/NumberOfEvents/NumberOfEvents";
 import EventList from "./components/EventList/EventList";
-import { mockData } from "./MockData/mock-data";
 import { extractLocations, getEvents } from "./api";
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
+    seletedLocation: "all",
+    eventCount: 32,
   };
 
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
-      this.setState({ events, locations: extractLocations(events) });
+      if (this.mounted) {
+        events = events.slice(0, this.state.eventCount);
+        this.setState({ events, locations: extractLocations(events) });
+      }
     });
   }
 
@@ -22,7 +26,35 @@ class App extends Component {
     this.mounted = false;
   }
 
-  updateEvents = (location) => {
+  updateEvents = (location, inputNumber) => {
+    const { eventCount, seletedLocation } = this.state;
+
+    if (location) {
+      getEvents().then((events) => {
+        const locationEvents =
+          location === "all"
+            ? events
+            : events.filter((event) => event.location === location);
+        const eventsToShow = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: eventsToShow,
+          seletedLocation: location,
+        });
+      });
+    } else {
+      getEvents().then((events) => {
+        const locationEvents =
+          seletedLocation === "all"
+            ? events
+            : events.filter((event) => event.location === seletedLocation);
+        const eventsToShow = location.slice(0, inputNumber);
+        this.setState({
+          events: eventsToShow,
+          eventCount: inputNumber,
+        });
+      });
+    }
+
     getEvents().then((events) => {
       const locationEvents =
         location === "all"
@@ -34,16 +66,26 @@ class App extends Component {
     });
   };
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(", ").shift();
+      return { city, number };
+    });
+
+    return data;
+  };
+
   render() {
-    const { locations, events } = this.state
+    const { locations, events } = this.state;
 
     return (
       <div className="App">
-        <CitySearch
-          locations={locations}
-          updateEvents={this.updateEvents}
-        />
-        <NumberOfEvents />
+        <CitySearch locations={locations} updateEvents={this.updateEvents} />
+        <NumberOfEvents updateEvents={this.updateEvents} />
         <EventList events={events} />
       </div>
     );
