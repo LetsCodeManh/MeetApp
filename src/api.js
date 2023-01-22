4bb1e32e6457c122d934b2372ef33d9846926bf5
LetsCodeManh <manh.ng@icloud.com>
2023-01-22 12:35:59 +0100

    Finish Api?

--- src/api.js
+++ src/api.js
@@ -1,6 +1,16 @@
 import axios from "axios";
-import nProgress from "nprogress";
+import NProgress from "nprogress";
 import { mockData } from "./MockData/mock-data";
+
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
 
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem("access_token");
@@ -16,7 +26,7 @@ export const getAccessToken = async () => {
       const results = await axios.get(
         "https://oviki6w0ab.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
       );
-      const { authUrl } = results.mockData;
+      const { authUrl } = results.data;
       return (window.location.href = authUrl);
     }
     return code && getToken(code);
@@ -24,10 +34,18 @@ export const getAccessToken = async () => {
   return accessToken;
 };
 
-export const extractLocations = (events) => {
-  let extractLocations = events.map((event) => event.location);
-  let locations = [...new Set(extractLocations)];
-  return locations;
+export const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    `https://oviki6w0ab.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
+
+  access_token && localStorage.setItem("access_token", access_token);
+  return access_token;
 };
 
 export const checkToken = async (accessToken) => {
@@ -40,9 +58,39 @@ export const checkToken = async (accessToken) => {
   return result;
 };
 
+export const getEvents = async () => {
+  NProgress.start();
+
+  if (isLocalhost) {
+    NProgress.done();
+    return mockData;
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url = `https://oviki6w0ab.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/${token}`;
+    const result = await axios.get(url);
+    if (result.data) {
+      let locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
+  }
+};
+
+export const extractLocations = (events) => {
+  let extractLocations = events.map((event) => event.location);
+  let locations = [...new Set(extractLocations)];
+  return locations;
+};
+
 export const removeQuery = () => {
   if (window.history.pushState && window.location.pathname) {
-    let newUrl =
+    var newUrl =
       window.location.protocol +
       "//" +
       window.location.host +
@@ -52,51 +100,4 @@ export const removeQuery = () => {
     newUrl = window.location.protocol + "//" + window.location.host;
     window.history.pushState("", "", newUrl);
   }
-};
-
-export const getToken = async (code) => {
-  try {
-    const encodeCode = encodeURIComponent(code);
-    const response = await fetch(
-      "https://oviki6w0ab.execute-api.eu-central-1.amazonaws.com/dev/api/token" +
-        "/" +
-        encodeCode
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const { access_token } = await response.json();
-    access_token && localStorage.setItem("access_token", access_token);
-  } catch (error) {
-    error.json();
-  }
-};
-
-export const getEvents = async () => {
-  nProgress.start();
-
-  if (window.location.href.startsWith("http://localhost")) {
-    nProgress.done();
-    return mockData;
-  }
-
-  const token = await getAccessToken();
-
-  if (token) {
-    removeQuery();
-    const url =
-      "https://oviki6w0ab.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" +
-      "/" +
-      token;
-    const result = await axios.get(url);
-    if (result.data) {
-      let locations = extractLocations(result.data.events);
-      localStorage.setItem("lastEvents", JSON.stringify(result.data));
-      localStorage.setItem("locations", JSON.stringify(locations));
-    }
-    nProgress.done();
-    return result.data.events;
-  }
-
-  return mockData;
 };
--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -10,13 +10,7 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://cra.link/PWA
 
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
-);
+
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
