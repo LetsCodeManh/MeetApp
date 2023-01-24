d94e643455352f489d619a7d027ee97dcae85266
LetsCodeManh <manh.ng@icloud.com>
2023-01-24 23:35:54 +0100

    Testing with Puppeteer

---
+++ src/__tests__/EndToEnd.test.js
@@ -0,0 +1,37 @@
+import puppeteer from "puppeteer";
+
+describe("show/hide an event details", () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250, // slow down by 250ms
+      ignoreDefaultArgs: ["--disable-extensions"], //ingonres default setting that causes timeout errors
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test("An event element is collapsed by default", async () => {
+    const eventDetails = await page.$(".event .description");
+    expect(eventDetails).toBeNull();
+  });
+
+  test("User can expand an event to see its details", async () => {
+    const eventDetails = await page.$(".event .description");
+    expect(eventDetails).toBeDefined();
+  });
+
+  test("User can collapse an event to hide its details", async () => {
+    await page.click(".event .expand-collapse-buttons");
+    const eventDetails = await page.$(".event .description");
+    expect(eventDetails).toBeNull();
+  });
+});
